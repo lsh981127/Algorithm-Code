99a42e129401e101202c92da23d844c519b6f64b
lsh981127 <61014396+lsh981127@users.noreply.github.com>
2024-03-19 13:07:13 +0900

    [Silver II] Title: 유기농 배추, Time: 56 ms, Memory: 31516 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,28 +1,30 @@
 import sys
 sys.setrecursionlimit(10000)
+input = sys.stdin.readline
 
-def dfs(x, y):
-    dx = [0, 0, -1, 1]
-    dy = [1, -1, 0, 0]
-    
+def dfs(x,y):
+    dx = [1, 0, -1, 0] # 하 좌 상 우
+    dy = [0, -1, 0, 1]
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if (0 <= nx < m) and (0 <= ny < n):
-            if field[ny][nx] == 1:
-                field[ny][nx] = 0
-                dfs(nx, ny)
-                
-for _ in range(int(sys.stdin.readline())):
-    m, n, k = map(int, sys.stdin.readline().split())
-    field = [[0 for _ in range(m)] for _ in range(n)]
-    count = 0
-    for _ in range(k):
-        x, y = map(int, sys.stdin.readline().split())
-        field[y][x] = 1
-    for x in range(m):
-        for y in range(n):
-            if field[y][x] == 1:
-                dfs(x, y)
-                count += 1
-    print(count)
+        if (0 <= nx < n) and (0 <= ny < m):
+            if arr[nx][ny] == 1:
+                arr[nx][ny] = 0
+                dfs(nx,ny)
+
+for _ in range(int(input())):
+    m, n, num = map(int, input().split())  # m : 가로, n : 세로
+    arr = [[0 for _ in range(m)] for _ in range(n)]  # 인접 행렬
+    result = 0
+    
+    for _ in range(num):
+        y, x = map(int, input().split())  # input 값으로 가로 index, 세로 index 들어와서 순서 바꿔줌
+        
+        arr[x][y] = 1
+    for i in range(n):
+        for j in range(m):
+            if arr[i][j] == 1:
+                dfs(i,j)
+                result +=1
+    print(result)
