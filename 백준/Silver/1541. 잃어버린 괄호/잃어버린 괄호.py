3f6866e22a5c937ee9476cd9307f314cba9a1a1a
lsh981127 <61014396+lsh981127@users.noreply.github.com>
2024-03-26 23:17:50 +0900

    코드 주석 추가.py

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -2,6 +2,11 @@ import sys
 
 input = sys.stdin.readline
 
+# 그리디 문제로 - 와 - 사이의 값들만 합치면 최솟값이 된다.
+# 1. 그렇기에 식 자체를 읽을때부터 - 기준으로 파싱한 뒤
+# 2. 나누어진 각각의 리스트들의 합을 구한고
+# 제일 처음의 값을 제외한 나머지 값들은 빼는 방법으로 구현하면 된다.
+
 equation = list(map(str, input().split('-')))
 
 total = 0
