74cdf95856290646b2e1ee3c5dc6ceb7f3f3e51e
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-04-10 22:16:57 +0900

    [Silver I] Title: IOIOI, Time: 356 ms, Memory: 32368 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -3,21 +3,21 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
-S = list(input().strip())
-arr = ['O'] * N
+S = input() # 리스트 안만들고 그냥 이렇게 해서 문자열로 분할하는 거였네
 
-P = list('I'+ 'I'.join(arr)+ 'I') # 1 ~ 01010 ~ 1
-
-length = 2*N + 1
-cursor = 0
+i = 0
+count = 0
 result = 0
-# print(P)
-# print(S)
-while cursor + length <= M:
-    temp = S[cursor: cursor + length]
-    if temp == P:
-        result += 1
-    
-    cursor += 1
+
+while i < (M-1):
+    if S[i:i+3] == "IOI":
+        i+=2
+        count += 1
+        if count == N:
+            result += 1
+            count -= 1
+    else:
+        i += 1
+        count = 0
 
 print(result)
