10afb58d07ead60f716b11099e475494b3941e6a
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-04-30 13:40:22 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+
+dp = [0] * (1001)
+dp[1], dp[2] = 1, 3
+
+for i in range(3, 1001):
+    if i % 2 == 0: # 짝수
+        dp[i] = dp[i-1] * 2 + 1
+    else: # 홀수
+        dp[i] = dp[i-1] * 2 - 1
+    
+
+print(dp[n] % 10007)
