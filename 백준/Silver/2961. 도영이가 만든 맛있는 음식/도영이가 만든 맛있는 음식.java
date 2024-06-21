311d659d05cd4f73866b3c382bbb5c861690666f
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-06-21 15:48:18 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 124 ms, Memory: 14132 KB -BaekjoonHub

---
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.java
@@ -0,0 +1,71 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+
+    static int num;
+    static int[] sour;
+    static int[] bitter;
+    static boolean[] visited;
+    static int min = Integer.MAX_VALUE;
+
+
+    /*static void cook(int cnt, int times, int sum) {
+        if (min < Math.abs(times - sum)) return;        // 이미 최솟값을 넘어서면
+        if (cnt == num && times == 0) return;           // 아무것도 선택 안한 경우
+        if(cnt == num) {
+            min = Math.min(min, Math.abs(times - sum));
+            return ;
+        }
+
+        cook(cnt + 1, times == 0 ? sour[cnt] : times*sour[cnt], sum + bitter[cnt]);
+        cook(cnt + 1, times, sum);      // 암것도 안넣을때
+    }*/
+
+    static void cook(int cnt) {
+
+        if(cnt == num) {
+            int times = 1;
+            int sum = 0;
+            int count = 0;
+            for(int i = 0; i < num; i++) {
+                if (visited[i]) {
+                    times *= sour[i];
+                    sum += bitter[i];
+                    count++;
+                }
+
+            }
+            if (count == 0) return;
+            min = Math.min(min, Math.abs(times - sum));
+            return ;
+        }
+
+        visited[cnt] = true;
+        cook(cnt + 1);
+        visited[cnt] = false;
+        cook(cnt + 1);      // 암것도 안넣을때
+    }
+
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        num = Integer.parseInt(br.readLine());
+        sour = new int[num];
+        bitter = new int[num];
+        visited = new boolean[num];
+
+        for(int i = 0; i < num; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            sour[i] = Integer.parseInt(st.nextToken());
+            bitter[i] = Integer.parseInt(st.nextToken());
+        }
+
+//        cook(0, "");
+        cook(0);
+        System.out.println(min);
+        br.close();
+    }
+}
