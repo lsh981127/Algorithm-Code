940f3bd97c428b110e975bce9b120861ab660960
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-06-21 13:24:41 +0900

    [Bronze II] Title: 백설 공주와 일곱 난쟁이, Time: 104 ms, Memory: 14096 KB -BaekjoonHub

---
+++ 백준/Bronze/3040. 백설 공주와 일곱 난쟁이/백설 공주와 일곱 난쟁이.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int[] num = new int[9];
+    static int[] arr = new int[7];
+
+    static void comb(int count, int start, int sum){
+        if (count == 7) {
+            if (sum == 100){
+                for(int i = 0; i < arr.length;i++)
+                    System.out.println(arr[i]);
+            }
+            return;
+        }
+
+        for (int i = start; i < num.length; i++) {
+            arr[count] = num[i];
+            sum += num[i];
+            comb(count+1, i+1, sum);
+            sum -= num[i];
+        }
+    }
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for(int i = 0; i < num.length; i++) {
+            num[i] = Integer.parseInt(br.readLine());
+        }
+
+        comb(0,0, 0);
+
+
+    }
+}
