54c1ec03c8538650ab76aa109b4e8f6dfebd8612
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-06-25 16:34:45 +0900

    [Silver V] Title: 단어 정렬, Time: 568 ms, Memory: 23208 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int num = Integer.parseInt(br.readLine());
+        String[] arr = new String[num];
+
+        for(int i = 0; i < num; i++) {
+            String temp = br.readLine();
+            arr[i] = temp;
+        }
+
+        Arrays.sort(arr, (String o1, String o2) -> o1.length() == o2.length() ? o1.compareTo(o2) : o1.length() - o2.length());
+
+        for(int i = 0; i < num; i++) {
+            if(i != 0){
+                if (arr[i].equals(arr[i-1])) {
+                    continue;
+                }
+            }
+            System.out.println(arr[i]);
+        }
+//        for(int i = 0; i < num; i++) System.out.println(arr[i]);
+    }
+}
