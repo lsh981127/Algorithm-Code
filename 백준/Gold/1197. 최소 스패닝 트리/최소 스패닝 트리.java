5fa2f47f36571ab06a5cff8bee10b7f7d1ce7543
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-03 14:43:38 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 564 ms, Memory: 53712 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        // 노드 갯수가 주어져야하는데
-        int N = sc.nextInt();
-        int M = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
         List<int[]>[] g = new List[N];
         for (int i = 0; i < N; i++) g[i] = new ArrayList<>();
 
@@ -21,21 +20,45 @@ public class Main {
             }
         };**/
         for(int i = 0; i < M;i++){
-            int from = sc.nextInt();
-            int to = sc.nextInt();
-            int cost = sc.nextInt();
-            g[from-1].add(new int[]{to-1, cost});
-            g[to-1].add(new int[]{from-1, cost});
+            st = new StringTokenizer(br.readLine(), " ");
+            int from = Integer.parseInt(st.nextToken());
+            int to = Integer.parseInt(st.nextToken());
+            int cost = Integer.parseInt(st.nextToken());
+            if(cost != 0) {
+                g[from-1].add(new int[]{to-1, cost});
+                g[to-1].add(new int[]{from-1, cost});    
+            }
+            
         }
 
         boolean[] v = new boolean[N];
         int[] minEdge = new int[N];
 //        Arrays.fill(minEdge, Integer.MAX_VALUE/2); // 메모리 터지는 거 방지하기 위해 2로 나눔
         for(int i = 0; i < N; i++) minEdge[i] = Integer.MAX_VALUE;
-
+        PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[1],o2[1]));
+        
         int sum = 0, cnt = 0;
         minEdge[0] = 0;
-        for(int i = 0; i < N; i++) {
+        pq.offer(new int[]{0, 0});   // 정점, 비용
+        
+        
+        while(!pq.isEmpty()) {
+            int[] cur = pq.poll();
+            int min = cur[1];
+            int minVertex = cur[0];
+
+            if(v[minVertex]) continue;          // 방문 여부를 검사해서 끊어줌으로써 사이클이 안만들어지도록!
+            v[minVertex] = true;
+            sum += min;
+            if(cnt++==N-1) break;
+            for(int[] j: g[minVertex]) {
+                if(!v[j[0]] && minEdge[j[0]] > j[1]) {
+                    minEdge[j[0]] = j[1];
+                    pq.offer(j);
+                }
+            }
+        }
+        /**for(int i = 0; i < N; i++) {
             int minVertex = -1;                 // 최소 가치를 가지는 노드 기록 변수
             int min = Integer.MAX_VALUE;        // 최소 가치
             for(int j = 0; j < N; j++) {
@@ -47,16 +70,16 @@ public class Main {
             v[minVertex] = true;
             sum += min;
 
-            if(cnt == N-1) break;
+            if(cnt++ == N-1) break;
             for(int[] j: g[minVertex]) {
                 if(!v[j[0]] && minEdge[j[0]] > j[1]) {
                     minEdge[j[0]] = j[1];
                 }
             }
 //            System.out.println(Arrays.toString(minEdge));
-        }
+        }**/
         System.out.println(sum);
-        sc.close();
+        br.close();
 
     }
 }
