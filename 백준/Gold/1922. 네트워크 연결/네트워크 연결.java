8e5f09f348b65c133d5fc0983b5736e3c3bae1f4
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-04 11:31:34 +0900

    [Gold IV] Title: 네트워크 연결, Time: 456 ms, Memory: 49916 KB -BaekjoonHub

--- 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
+++ 백준/Gold/1922. 네트워크 연결/네트워크 연결.java
@@ -1,62 +1,58 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    static int[] p;
-    static int N;
-    static int[][] g;
-
-
-    static void make() {
-        p = new int[N];
-        for(int i = 0; i < N; i++) p[i]=i;
-    }
-    static int find(int a) {
-        if(p[a] == a) return a;
-        return p[a] = find(p[a]);
-    }
-
-    static boolean union(int a, int b) {
-        int aRoot = find(a);
-        int bRoot = find(b);
-
-        if(aRoot == bRoot) return false;
-        p[bRoot] = aRoot;
-        return true;
-    }
-
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
-        N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
-        // 입력값이 from to cost 형태 => 인접리스트로 하기 쉬움 => 크루스칼
         boolean[] visited = new boolean[N];
-        g = new int[M][3];
+
+        List<int[]>[] li = new List[N];
+        for(int i = 0; i < N; i++) li[i] = new ArrayList<>();
+
+
         for(int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
             int from = Integer.parseInt(st.nextToken())-1;
             int to = Integer.parseInt(st.nextToken())-1;
             int cost = Integer.parseInt(st.nextToken());
-            g[i] = new int[]{from, to, cost};
+            li[from].add(new int[]{to, cost});
+            li[to].add(new int[]{from, cost});
         }
 
-        Arrays.sort(g, (o1,o2) -> Integer.compare(o1[2], o2[2])); // 가중치 기준 정렬
-        make();
         int sum = 0, count = 0;     // sum : 비용용도, count : 종료용 간선 갯수 측정용
+        int[] w = new int[N];
+        for(int i = 0; i < N; i++) w[i] = Integer.MAX_VALUE;
 
-        for(int[] temp : g) {
-            int from = temp[0];
-            int to = temp[1];
-            if(union(to, from)) {           // 합쳐지면
-                sum += temp[2];
-                count++;
+//        for(int i = 0; i < N; i++) {
+//            for(int[] k: li[i]) {
+//                System.out.print(Arrays.toString(k) + " ");
+//            }
+//            System.out.println();
+//        }
+
+        w[0] = 0;
+        for(int i = 0; i < N; i++) {
+            int minVertex = -1;
+            int min = Integer.MAX_VALUE;
+            for(int j = 0; j < N; j++) {
+                if(!visited[j] && min > w[j]) {
+                    min = w[j];
+                    minVertex = j;
+                }
             }
-            if(count == N-1) break;
-
+            visited[minVertex] = true;
+            sum += min;
+            if(count++ == N - 1) break;
+            for(int[] k: li[minVertex]) {
+                if(!visited[k[0]] && w[k[0]] > k[1]) {
+                    w[k[0]] = k[1];
+                }
+            }
         }
-
         System.out.println(sum);
         br.close();
     }
