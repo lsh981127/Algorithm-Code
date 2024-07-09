374fa0d99f34437c9023ff8a1710d69bad2bcbeb
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-07-09 11:24:40 +0900

    [level 2] Title: 전화번호 목록, Time: 187.18 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/README.md
+++ 프로그래머스/2/42577. 전화번호 목록/README.md
@@ -1,10 +1,10 @@
 # [level 2] 전화번호 목록 - 42577 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42577) 
 
 ### 성능 요약
 
-메모리: 133 MB, 시간: 191.93 ms
+메모리: 133 MB, 시간: 187.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 09일 10:08:27
+2024년 07월 09일 11:24:38
 
 ### 문제 설명
 
--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -7,7 +7,11 @@ class Solution {
             }
 
             for (int i = 0; i < phone_book.length; i++) {
+                // 각 값들에 대해
                 for (int j = 0; j < phone_book[i].length(); j++) {
+                    // 길이 0부터 해당 길이까지 해당 값들의 Key가 존재하는지 확인하는 거야
+                    // O(N^2)라서 다른 방법 없나 고민했는데 더 좋은 아이디어가 없어서 ㅠ
+                    // 1,000,000 ^ 2  = 10^12
                     if (set.contains(phone_book[i].substring(0, j))) {
                         return false;
                     }
