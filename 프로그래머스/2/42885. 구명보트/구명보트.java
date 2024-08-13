b776527ea8d8306a84f924cc4721830b30a14f52
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2024-08-13 13:40:07 +0900

    [level 2] Title: 구명보트, Time: 10.53 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -6,6 +6,8 @@ class Solution {
         // 반례 존재, 30 50 50 70
         // 작은 애를 뽑았으면 채울 수 있는 애들 중 가장 큰 애랑 태우기
         
+        //https://velog.io/@seonydg/%EC%BD%94%ED%85%8C-%EA%B7%B8%EB%A6%AC%EB%94%94Greedy-%EA%B5%AC%EB%AA%85%EB%B3%B4%ED%8A%B8%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%98%EB%A8%B8%EC%8A%A4
+        
         int answer = 0;
         int left= 0;
         int right = people.length - 1;
