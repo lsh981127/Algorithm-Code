6d62ff6c11295017efedae8f83b46d3ca9a2bdbf
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2025-01-16 22:22:32 +0900

    [Gold IV] Title: 알파벳, Time: 892 ms, Memory: 15344 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.java
+++ 백준/Gold/1987. 알파벳/알파벳.java
@@ -1,8 +1,8 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static char[][] graph;
-    static HashSet<Integer> set = new HashSet<>();
+    static int[][] graph;
+    static boolean[] visited = new boolean[26];
     static int result = 0;
     static int[] dx = {1, -1, 0, 0};
     static int[] dy = {0, 0, 1, -1};
@@ -13,33 +13,30 @@ public class Main {
         R = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        graph = new char[R][C];
+        graph = new int[R][C];
         for(int i = 0; i < R; i++) {
-            char[] temp = br.readLine().toCharArray();
+            String temp = br.readLine();
             for(int j = 0; j < C; j++) {
-                graph[i][j] = temp[j];
+                graph[i][j] = temp.charAt(j) - 'A';
             }
         }
-
-        set.add(graph[0][0] - 'A');
+        
         dfs(0, 0, 1);
         System.out.println(result);
     }
 
     static void dfs(int x, int y, int count) {
-        set.add(graph[x][y] - 'A');
+        visited[graph[x][y]] = true;
         result = Math.max(result, count);
         for(int i = 0; i < 4; i++) {
             int tx = x + dx[i];
             int ty = y + dy[i];
             if(tx >= 0 && tx < R && ty >= 0 && ty < C) {
-                int val = graph[tx][ty] - 'A';
-                if(!set.contains(val)) {
+                if(!visited[graph[tx][ty]]) {
                     dfs(tx, ty, count + 1);
-                    set.remove(val);
+                    visited[graph[tx][ty]] = false;
                 }
             }
         }
     }
-
 }
