e1211ef6fcc0e57f2c43518639c43e628098a5de
Sean Lee <61014396+lsh981127@users.noreply.github.com>
2025-01-17 13:53:15 +0900

    [Gold IV] Title: 별 찍기 - 11, Time: 376 ms, Memory: 120776 KB -BaekjoonHub

---
+++ 백준/Gold/2448. 별 찍기 － 11/별 찍기 － 11.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N;
+    static char[][] map;
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        map = new char[N][2*N - 1];
+        for(char[] i : map) {
+            Arrays.fill(i, ' ');
+        }
+
+        star(0, N - 1, N);
+
+        StringBuilder sb = new StringBuilder();
+        for(char[] i : map) {
+            sb.append(i);
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void star(int x, int y, int count) {
+        if(count == 3) {
+            map[x][y] = '*';
+            map[x+1][y-1] = map[x+1][y+1] = '*';
+            map[x+2][y-2] = map[x+2][y-1] = '*';
+            map[x+2][y] = map[x+2][y+2] = map[x+2][y+1] = '*';
+            return;
+        }
+
+        int div = count / 2;
+        star(x, y, div);
+        star(x + div, y - div, div);
+        star(x + div, y + div, div);
+    }
+
+}
